a467a73d7efdbaf0f1b03104f991bcec289a630d
jos2064 <168182477+jos2064@users.noreply.github.com>
2025-11-25 12:48:20 +0530

    Update and rename portScannerAsyn.py to Async_portScanner.py

--- Async_portScanner.py
+++ Async_portScanner.py
@@ -1,26 +1,3 @@
-# import asyncio
-# async def scan_port(ip,port):
-#     try:
-#         reader, writer = await asyncio.wait_for(asyncio.open_connection(ip,port),timeout=0.5)
-#         print  (f"Port {port} is open")
-#         writer.close()
-#         await writer.wait_closed()
-#     except:
-#         pass
-    
-# async def scan(ip,start_port,end_port):
-#     tasks = []
-#     for port in range(start_port,end_port + 1):
-#         tasks.append(scan_port(ip,port))
-
-#     await asyncio.gather(*tasks)
-
-# ip = input("Enter target IP:")
-# start = int(input("Enter start port:"))
-# end = int(input("Enter end port:"))
-
-# asyncio.run(scan(ip,start,end))
-
 import asyncio
 open_ports = []
 async def scan_port(ip,port):
@@ -56,5 +33,6 @@ def save_results(ip):
 ip = input("Enter IP:")
 start = int(input("Enter start port:"))
 end = int(input("Enter end port:"))
+
 
 asyncio.run(scan(ip,start,end))
